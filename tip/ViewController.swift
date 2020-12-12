c6b1c406b0495d742dbf06dd9fdea64aef925138
kandarpajvalia <kandarpa9@gmail.com>
2020-12-12 18:22:54 -0500

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Kandarp Ajvalia on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
