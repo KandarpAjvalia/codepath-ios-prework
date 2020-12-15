033484f118db9c709e5582cf2c69bf3938c3f4a3
kandarpajvalia <kandarpa9@gmail.com>
2020-12-14 20:20:10 -0500

    added settings screen storyboard and view controller

---
+++ tip/SettingsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsViewController.swift
+//  tip
+//
+//  Created by Kandarp Ajvalia on 12/14/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+
+}
