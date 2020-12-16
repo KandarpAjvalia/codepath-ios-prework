4476b01ff8a482526e9e4102dcba80fead0587b4
kandarpajvalia <kandarpa9@gmail.com>
2020-12-16 14:53:18 -0500

    added feature where the bill amount is saved if elapsed time is less than 10 minutes

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -13,23 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet var tipView: UIView!
     
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let defaults = UserDefaults.standard
-        tipControl.selectedSegmentIndex = defaults.integer(forKey: "defaultTipIdx")
-        
-        self.billAmountTextField.keyboardType = .decimalPad
-    }
 
+        billAmountTextField.text = self.getDefaultBill()
+        tipControl.selectedSegmentIndex = defaults.integer(forKey: "defaultTipIdx")
+        self.calculateTipHelper()
+        self.billAmountTextField.keyboardType = .decimalPad
+
+    }
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
+        defaults.set(Date(), forKey: "defaultBillTime")
+        self.calculateTipHelper()
+    }
+    
+    func calculateTipHelper() -> Void {
         let bill = Double(billAmountTextField.text!) ?? 0
+        defaults.set(bill, forKey: "defaultBill")
+
         let tipPercentages = [0.15, 0.18, 0.2]
         
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
@@ -37,6 +47,19 @@ class ViewController: UIViewController {
         
         tipPercentageLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    func getDefaultBill() -> String {
+        let calendar = Calendar.current
+        let billTime = (defaults.object(forKey: "defaultBillTime") ?? Date()) as! Date
+        let currentTime = Date()
+        
+        let components = calendar.dateComponents([.minute], from: billTime, to: currentTime)
+        if Int(components.minute ?? 0) > 10 {
+            defaults.set(0, forKey: "defaultBill")
+        }
+                
+        return String(defaults.double(forKey: "defaultBill"))
     }
 }
 
